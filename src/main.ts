c496b9a17d4882808348aa5e35da1c013af2acf3
willy418785 <spdf7785gh>
2020-08-09 21:19:37 +0800

    feat✨(main.ts): add Events
    add BuffGameEvent, NewItemGameEvent, StoryTellingGameEvent, ShoppingGameEvent

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log("Hello World");
+import { GameEvent } from './GameEvent'
+
+class BuffGameEvent extends GameEvent {
+    public evaluate(player, level): number {
+        let hpRatio = player.hp / player.maxHp;
+        let atkRatio = player.atk / player.maxAtk;
+        let defRatio = player.def / player.maxDef;
+        return 1 - (hpRatio + atkRatio + defRatio) / 3;
+    }
+}
+class NewItemGameEvent extends GameEvent {
+    public evaluate(player, level): number {
+        return 1 - (player.itemList.length / player.maxNumOfItems);
+    }
+}
+class StoryTellingGameEvent extends GameEvent {
+    public evaluate(player, level): number {
+        return Math.pow(level.progression / 100, 2);
+    }
+}
+class ShoppingGameEvent extends GameEvent {
+    public evaluate(player, level): number {
+        return player.lux / player.maxLux;
+    }
+}
