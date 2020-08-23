b1f862fdc2fb5e787bc3d2fc16f671601d498125
pomodorozhong <pomodorozhong@gmail.com>
2020-08-23 19:28:44 +0800

    feat✨(keystrokeHandler): simplify the input classification rule

--- src/model/keystrokeHandler.ts
+++ src/model/keystrokeHandler.ts
@@ -1,15 +1,11 @@
 export class KeystrokeHandler {
   minimal_valid_duration: number;
-  max_offset: number;
   previous_stroke: string;
-  max_multiplier: number;
   classes = [".", "_"];
 
   constructor() {
     this.minimal_valid_duration = 300;
-    this.max_offset = 80;
     this.previous_stroke = "";
-    this.max_multiplier = 2;
   }
 
   newStroke(duration: number): string {
@@ -19,7 +15,7 @@ export class KeystrokeHandler {
       this.previous_stroke = "";
       strokeClass = "";
     } else {
-      strokeClass = this.classes[classIndex - 1];
+      strokeClass = this.classes[classIndex];
     }
 
     const stroke_array: string = this.previous_stroke + strokeClass;
@@ -29,15 +25,10 @@ export class KeystrokeHandler {
 
   strokeClassifier(duration): number {
     let class_: number = NaN;
-    for (let multiplier = 1; multiplier <= this.max_multiplier; multiplier++) {
-      const base = multiplier * this.minimal_valid_duration;
-      const lower_bound = base - this.max_offset;
-      const upper_bound = base + this.max_offset;
-
-      if (duration > lower_bound && duration < upper_bound) {
-        class_ = multiplier;
-        break;
-      }
+    if (duration < this.minimal_valid_duration) {
+      class_ = 0;
+    } else {
+      class_ = 1;
     }
 
     return class_;
